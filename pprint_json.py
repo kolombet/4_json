f90c4e666f59609e493eb27d28f676e3d0b6e814
kko <>
2018-03-04 19:51:05 +0300

    refactoring

--- pprint_json.py
+++ pprint_json.py
@@ -8,7 +8,6 @@ import sys
 def load_data(filepath):
     with codecs.open(filepath, "r", "utf-8") as json_file:
         json_content = json_file.read()
-        json_file.close()
         try:
             return json.loads(json_content)
         except json.decoder.JSONDecodeError:
@@ -20,24 +19,23 @@ def pretty_print_json(parsed_json):
                      ensure_ascii=False))
 
 
-def get_json_path():
+def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", "--file", dest="json_path",
-                        help="custom json file path")
-    args = parser.parse_args()
-    json_path = args.json_path
-    if json_path is None:
-        json_path = "raw.json"
-    return json_path
+    parser.add_argument(
+        "-f",
+        "--file",
+        dest="json_path",
+        help="custom json file path",
+        default="raw.json"
+    )
+    return parser.parse_args()
 
 
-if __name__ == '__main__':
-    json_path = get_json_path()
+if __name__ == "__main__":
+    json_path = get_args().json_path
     if not os.path.isfile(json_path):
-        print("error: can't find file {}".format(json_path))
-        sys.exit()
+        sys.exit("error: can't find file {}".format(json_path))
     parsed_json = load_data(json_path)
     if not parsed_json:
-        print("file content is not valid json")
-        sys.exit()
+        sys.exit("file content is not valid json")
     pretty_print_json(parsed_json)
